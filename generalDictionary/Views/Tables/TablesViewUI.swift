cf5f9e2d62fe2836a61aa355f2010fe2f2e53dc9
Adrian_Velazquez <adrian.velazquezo96@gmail.com>
2023-01-09 18:50:39 -0600

    [UPDATE] integracion vista de tablas

--- generalDictionary/Views/Tables/TablesViewUI.swift
+++ generalDictionary/Views/Tables/TablesViewUI.swift
@@ -28,6 +28,16 @@ class TablesViewUI: UIView{
         return tableView
     }()
     
+    private lazy var infoLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .left
+        label.text = "Hace referencia a una vista que representa unos datos por medio del uso de filas, las cuales se encuentran ubicadas en una sola columna. \n\nvar elementList: [String] = [”Elemento1”, ”Elemento2”, ”Elemento3”] \n\nSe declara de la siguiente manera: \n\npublic lazy var nombreDeLaTabla: UITableView = { \nlet tableView = UITableView() \ntableView.register(exampleTableViewCell.self, forCellReuseIdentifier: exampleTableViewCell.identifier) \ntableView.translatesAutoresizingMaskIntoConstraints = false \ntableView.delegate = self \ntableView.dataSource = self \ntableView.backgroundColor = .white \nreturn tableView \n}()"
+        label.numberOfLines = 0
+        label.textColor = .black
+        return label
+    }()
+    
     public convenience init(
         navigation: UINavigationController,
         delegate: TablesViewUIDelegate){
@@ -49,15 +59,20 @@ class TablesViewUI: UIView{
     
     func setUI(){
         self.backgroundColor = .white
+        self.addSubview(infoLabel)
         self.addSubview(tableView)
     }
     
     func setConstraints(){
         NSLayoutConstraint.activate([
-            tableView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
-            tableView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
-            tableView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
-            tableView.bottomAnchor.constraint(equalTo: self.bottomAnchor),
+            infoLabel.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
+            infoLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 20),
+            infoLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -20),
+            
+                tableView.topAnchor.constraint(equalTo: infoLabel.bottomAnchor, constant: 10),
+                tableView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+                tableView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+                tableView.bottomAnchor.constraint(equalTo: self.bottomAnchor),
         ])
     }
 }
